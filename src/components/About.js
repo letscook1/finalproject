54507edf7d649afef2af5a25880c7d3335c3a9ae
Fuzzy Hair <pamelasanchezhernandez@Pamelas-MacBook-Pro.local>
2019-07-01 21:03:15 -0400

    updates

---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import logo from '../dog_01.png';
+import logo2 from '../images/Path 358.png';
+import logo3 from '../images/Title Bullet Points Icon.png';
+import '../About.css';
+
+class About extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header about">
+           <section className="Service">
+          <div className="logo3">
+          <img src={logo3} className="" alt="logo" />
+          <img src={logo2} className="App-logo2" alt="logo" />
+          </div>
+          <div className="logo"><img src={logo} alt="logo" /></div>
+          <button type="button" className="btn btn-secondary">Next</button>
+        
+       
+        </section>
+      </header>
+    </div>
+    );
+  }
+}
+
+export default About;
