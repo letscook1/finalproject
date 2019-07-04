397801bc150fda53e61e55f91c7e206ec5dcf72c
Fuzzy Hair <pamelasanchezhernandez@Pamelas-MBP.lan1>
2019-07-04 00:09:39 -0400

    updates home page and routes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import logo from '../images/woofPack__logo.png';
+import logo from '../images/bg.png';
+import wpLogo from '../images/wpLogo.png';
+import nav from '../images/nav.png';
 import '../App.css';
 
 class Home extends Component {
@@ -9,11 +11,25 @@ class Home extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-            Sign in with <a href="http://www.facebook.com">Facebook.</a>
-            </p>
-            <button type="button" className="btn btn-secondary">Book a Service</button>
-            <a href="/About">test</a>
+              <div className="App-logo">
+                <img src={wpLogo} className="App-logoImg" alt="logo"/>            
+              </div>
+              <div className="App-Nav">
+                <nav>
+                  <a href="/#/">About Us</a> |
+                  <a href="/#/">Reviews</a> |
+                  <a href="/#/">Services</a> |
+                  <a href="/#/">Sign Up</a>
+                </nav>
+                
+              </div>
+            <div className="App-HomeText">
+              <p>We care for your furry little loved ones while you're away</p>
+            </div>
+            
+             <a href="/About">
+            <button type="button" className="btn btn-light">Book a Service</button>
+            </a>
           
           </header>
         </div>
