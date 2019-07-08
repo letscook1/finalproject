ba0bd30ff0425e6d0043efe0feb8071b859ff605
letscook1 <letscook1@hotmail.com>
2019-07-08 15:33:53 -0400

    adding more files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import Main from './components/Main';
-const App = () => (
-  <div>
-    <Main />
-  </div>
+import { ConnectedRouter } from 'connected-react-router';
+import { configureStore, getHistory } from 'modules/store';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import RoutesComponent from 'view/shared/routes/RoutesComponent';
+import { LocaleProvider } from 'antd';
+import { getAntdLanguage } from 'i18n';
 
-)
+const store = configureStore();
 
-
-
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <LocaleProvider locale={getAntdLanguage()}>
+        <Provider store={store}>
+          <ConnectedRouter history={getHistory()}>
+            <RoutesComponent />
+          </ConnectedRouter>
+        </Provider>
+      </LocaleProvider>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
+import App from 'App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { unregister } from 'registerServiceWorker';
+import SettingsService from 'modules/settings/settingsService';
+import { i18n } from 'i18n';
 
-ReactDOM.render(
-<BrowserRouter>
-    <App />
-</BrowserRouter>
-    
-, document.getElementById('root'));
+(async function() {
+  document.title = i18n('app.title');
+  await SettingsService.fetchAndApply();
 
-serviceWorker.unregister();
+  ReactDOM.render(<App />, document.getElementById('root'));
+
+  /**
+   * It may cause cache issues when developing, so, after your app is ready,
+   * change those lines
+   */
+  unregister();
+  // registerServiceWorker();
+})();
